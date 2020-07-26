7922c1311c1d3104b7dbdb9185bb62a949951a0f
lm <710450868@qq.com>
2020-07-26 10:43:03 +0800

    deploy

---
+++ precache-manifest.59faa84b9d6c9c4bb9885f26669c32c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67905330b1ea4016233f81e2723d3f6",
+    "url": "/promise-website/index.html"
+  },
+  {
+    "revision": "f18f888e6b1f34801e12",
+    "url": "/promise-website/static/css/main.73404b99.chunk.css"
+  },
+  {
+    "revision": "5965a9102aada65f6b7f",
+    "url": "/promise-website/static/js/2.d3779de8.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/promise-website/static/js/2.d3779de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18f888e6b1f34801e12",
+    "url": "/promise-website/static/js/main.a3af1318.chunk.js"
+  },
+  {
+    "revision": "cebb207a7a013b18b052",
+    "url": "/promise-website/static/js/runtime-main.25c26719.js"
+  }
+]);
